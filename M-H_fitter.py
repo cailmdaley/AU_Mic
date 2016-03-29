2d45b31cd6fb890fb7ac2c81d7a1e1265c7d376d
cailmdaley <cailmdaley@gmail.com>
2016-03-29 13:51:25 -0400

    Fixed Metropolis-Hastings code

--- M-H_fitter.py
+++ M-H_fitter.py
@@ -1,8 +1,8 @@
 #This code is my attempt to write my own Metropolis-Hastings algorithm, and apply it to fake data.
 import numpy as np
+import math
 import numpy.random as rand
 import matplotlib.pyplot as plt
-plt.close()
 
 #Create randomly sampled parabola
 x = 10 * rand.random((100,)) - 5
@@ -17,49 +17,84 @@ def chi(a,b):
 #M-H Algorithim
 a = [5.]
 b= [6.]
-steps=1000
-accept= []
+steps = 10000
+a_accept = []
+alength = []
+b_accept = []
+blength = []
 chisquared = []
-sigma_a = 1
-sigma_b = 1
+sigma_a = 0.05
+sigma_b = 0.5
 for i in range(steps):
     if rand.random(1) < 0.5:
+        alength.append(1)
         a.append(rand.normal(a[-1], sigma_a))
         b.append(b[-1])
-        deltachi = chi(a[-2], b[-2]) - chi(a[-1], b[-1])
-        if deltachi < 0:
+        deltachi = chi(a[-1], b[-1]) - chi(a[-2], b[-2])
+        if deltachi > 0:
             if rand.random(1) <  np.exp(-deltachi/2):
                 a[-1] = a[-1]
-                accept.append(1)
+                a_accept.append(1)
             else: a[-1] = a[-2]
         else:
             a[-1] = a[-1]
-            accept.append(1)
+            a_accept.append(1)
     else:
+        blength.append(1)
         b.append(rand.normal(b[-1], sigma_b))
         a.append(a[-1])
-        deltachi = chi(a[-2], b[-2]) - chi(a[-1], b[-1])
-        if deltachi < 0:
+        deltachi = chi(a[-1], b[-1]) - chi(a[-2], b[-2])
+        if deltachi > 0:
             if rand.random(1) <  np.exp(-deltachi/2):
                 b[-1] = b[-1]
-                accept.append(1)
+                b_accept.append(1)
             else: b[-1] = b[-2]
         else:
             b[-1] = b[-1]
-            accept.append(1)
+            b_accept.append(1)
     chisquared.append(chi(a[-1], b[-1]))
-acceptance = len(accept)/np.float(steps)
+a_acceptance = len(a_accept)/np.float(len(alength))
+b_acceptance = len(b_accept)/np.float(len(blength))
+acceptance = (len(a_accept) + len(b_accept))/np.float(steps)
+print a_acceptance
+print b_acceptance
 print acceptance
 
 #Model & Data Plotting
-model = a[-1]*x**2 + b[-1]
-xindices = x.argsort()
-xsorted = x[xindices]
-modelsorted = model[xindices]
-plt.scatter(x, data,)
-plt.plot (xsorted, modelsorted)
-plt.show(False)
+def fitplot():
+    plt.close()
+    model = a[-1]*x**2 + b[-1]
+    xindices = x.argsort()
+    xsorted = x[xindices]
+    modelsorted = model[xindices]
+    plt.scatter(x, data,)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot (xsorted, modelsorted)
+    plt.show(False)
 
 #Chi^2 Plotting
-# plt.plot(range(steps),chisquared)
-# plt.show(False)
+def chiplot():
+    plt.close()
+    plt.loglog(range(steps),chisquared)
+    plt.xlabel('steps (log)')
+    plt.ylabel(r'$\chi^2$ (log)')
+    plt.show(False)
+
+#Histogram
+def histplot():
+    plt.close()
+    binsize = 20
+    a_hist = np.histogram(a, bins=binsize)
+    height = math.ceil(max(a_hist[1]) - math.floor(min(a_hist[1])))
+    height = height/len(a_hist[0])
+
+    ax1= plt.subplot(2,1,1)
+    ax1.barh(a_hist[1][:-1],a_hist[0], height=height)
+    plt.xlabel('a-value')
+    plt.ylabel('frequency')
+    ax2=plt.subplot(2,1,2)
+    ax2.hist(b,bins=binsize)
+    plt.xlabel('b-value')
+    plt.ylabel('frequency')
+    plt.show(False)
