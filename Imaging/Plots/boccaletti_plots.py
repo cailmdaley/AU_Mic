dfd91ae59c7ed92845b994ad84497ae81bc0cec1
cailmdaley <cailmdaley@gmail.com>
2017-03-20 22:50:35 -0400

    eep

---
+++ Imaging/Plots/boccaletti_plots.py
@@ -0,0 +1,107 @@
+from scipy.ndimage.interpolation import rotate
+from scipy.optimize import curve_fit
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+# image = 'aumic_18aug_usermask_natural'
+# image = 'aumic_24jun_usermask_natural'
+image = 'aumic_usermask_natural'
+
+# Read the header from the observed FITS continuum image:
+head = fits.getheader(image + ".fits")
+# Read in images and rotate so that disk is horizontal
+angleSE = 129.5 - 90
+im = rotate(fits.getdata(image + ".fits").squeeze(),
+            angleSE, reshape=False) * 1e6
+
+# Generate x and y axes: offset position in arcsec
+nx = head['NAXIS1']
+xpix = head['CRPIX1']
+xdelt = head['CDELT1']
+# print(head['CRVAL1'])
+# print(head['CRPIX1'])
+
+ny = head['NAXIS2']
+ypix = head['CRPIX2']
+ydelt = head['CDELT2']
+
+# Convert from degrees to arcsecs
+ra = ((np.arange(nx) - xpix + 1) * xdelt) * 3600
+dec = ((np.arange(ny) - ypix + 1) * ydelt) * 3600
+xaxis = ra[1:257][::-1]
+# print(ra[0],ra[-1])
+# print(dec[256])
+
+SE_midplane_intensity = np.zeros(len(xaxis))
+NW_midplane_intensity = np.zeros(len(xaxis))
+for i in range(len(xaxis)):
+    NW_midplane_intensity[i] = np.mean(im[255:258, 256 + i])
+    SE_midplane_intensity[i] = np.mean(im[255:258, 256 - i])
+
+
+# Define model Gaussian function:
+def gauss(x, *p):
+    A, mu, sigma = p
+    return A * np.exp(-(x - mu)**2 / (2. * sigma**2))
+
+p0 = [100., 0., 0.3]
+bounds = ([-50, -0.5, 0], [400, 0.5, 1])
+NW_amps, NW_mus, NW_sigmas = np.zeros(
+    len(xaxis)), np.zeros(len(xaxis)), np.zeros(len(xaxis))
+SE_amps, SE_mus, SE_sigmas = np.zeros(
+    len(xaxis)), np.zeros(len(xaxis)), np.zeros(len(xaxis))
+
+for i in range(len(xaxis)):
+    (SE_amps[i], SE_mus[i], SE_sigmas[i]), SE_var_matrix = curve_fit(gauss, dec[
+        256 - 35:256 + 35], im[256 - 35:256 + 35, 256 - i], p0=p0, bounds=bounds)
+    (NW_amps[i], NW_mus[i], NW_sigmas[i]), NW_var_matrix = curve_fit(gauss, dec[256-35:256+35], im[256-35:256+35,256+i], p0=p0, bounds=bounds)
+
+# Subtract beam FWHM
+bmin = head['bmin'] * 3600.
+bmaj = head['bmaj'] * 3600.
+bpa = head['bpa']
+b_FWHM = 2* bmin * bmaj / \
+    np.sqrt((bmin * np.cos(bpa + angleSE - 90))**2 +
+            (bmaj * np.sin(bpa + angleSE - 90))**2)
+
+SE_disk_FWHM = np.sqrt((2*SE_sigmas)**2 - b_FWHM**2)
+NW_disk_FWHM = np.sqrt((2*NW_sigmas)**2 - b_FWHM**2)
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, figsize=(8, 12), sharex=True)
+sns.set_style('whitegrid')
+
+ax1.set_title("Midplane intensity")
+ax1.set_ylabel(r"Surface brightness ($\mu$Jy/beam)")
+ax1.set_xlim(0, 5)
+ax1.set_ylim(-50, 350)
+ax1.plot(xaxis, SE_midplane_intensity, label='SE')
+ax1.plot(xaxis, NW_midplane_intensity, '--', label='NW')
+legend1 = ax1.legend()
+
+ax2.set_title("Spine Intensity")
+ax2.set_ylabel(r"Surface brightness ($\mu$Jy/beam)")
+ax2.plot(xaxis, SE_amps, label='SE')
+ax2.plot(xaxis, NW_amps, '--', label='NW')
+ax2.set_ylim(-50, 350)
+legend2 = ax2.legend()
+
+ax3.set_title("Spine deviation from midplane")
+ax3.set_ylabel("Elevation (\")")
+ax3.plot(xaxis, SE_mus, label='SE')
+ax3.plot(xaxis, NW_mus, '--', label='NW')
+ax3.set_ylim(-0.5, 0.5)
+legend3 = ax3.legend()
+
+ax4.set_xlabel("Projected seperation from star (\")")
+ax4.set_title("Disk Height")
+ax4.set_ylabel(r'Fit $\sigma$ (")')
+ax4.plot(xaxis, SE_sigmas, label='SE')
+ax4.plot(xaxis, NW_sigmas, '--', label='NW')
+ax4.set_ylim(0, 0.5)
+legend4 = ax4.legend()
+plt.suptitle("Composite")
+fig.savefig("boccaletti_plots_composite.png")
+plt.show()
---
+++ Imaging/Plots/contourPlotter.py
@@ -0,0 +1,219 @@
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.patches import Ellipse
+from matplotlib.ticker import MultipleLocator, LinearLocator, AutoMinorLocator
+import matplotlib.patheffects as PathEffects
+import matplotlib.pyplot as plt
+from astropy.io import fits
+import seaborn as sns
+import numpy as np
+
+
+def return_axis(ax, image, cpal, cbmin, cbmax, cbtmj, cbtmn, rms, cont_levs, text=None, residuals=None, axislabels=True):
+
+    # Read in FITS image and header:
+    head = fits.getheader(image + ".fits")
+    im = fits.getdata(image + ".fits").squeeze()
+    if residuals:
+        resid = fits.getdata(residuals + ".fits").squeeze()
+
+    # Generate x and y axes: offset position in arcsec
+    nx = head['NAXIS1']
+    xpix = head['CRPIX1']
+    xval = head['CRVAL1']
+    xdelt = head['CDELT1']
+
+    ny = head['NAXIS2']
+    ypix = head['CRPIX2']
+    yval = head['CRVAL2']
+    ydelt = head['CDELT2']
+
+    # Convert from degrees to arcsecs
+    ra = ((np.arange(nx) - xpix + 1) * xdelt) * 3600
+    dec = ((np.arange(ny) - ypix + 1) * ydelt) * 3600
+    ras = np.arange(xval - xpix*xdelt, xval + (nx-xpix)*xdelt, nx)
+
+    # Set axes limits
+    xmin = -5.0
+    xmax = 5.0
+    ymin = -5.0
+    ymax = 5.0
+    ax.set_xlim(xmax, xmin)
+    ax.set_ylim(ymin, ymax)
+    ax.grid(False)
+
+    # Set x and y major and minor tics
+    majorLocator = MultipleLocator(1)
+    ax.xaxis.set_major_locator(majorLocator)
+    ax.yaxis.set_major_locator(majorLocator)
+
+    minorLocator = MultipleLocator(0.2)
+    ax.xaxis.set_minor_locator(minorLocator)
+    ax.yaxis.set_minor_locator(minorLocator)
+
+    # Set x and y labels
+    if axislabels==True:
+        ax.set_xlabel(r'$\Delta \alpha$ (")', fontsize=15)
+        ax.set_ylabel(r'$\Delta \delta$ (")', fontsize=15)
+        ax.xaxis.set_ticklabels(
+            ['', '', '-4', '', '-2', '', '0', '', '2', '', '4', ''], fontsize=13)
+        ax.yaxis.set_ticklabels(
+            ['', '', '-4', '', '-2', '', '0', '', '2', '', '4', ''], fontsize=13)
+    else:
+        ax.xaxis.set_ticklabels([])
+        ax.yaxis.set_ticklabels([])
+
+    # Set physical range of colour map
+    cxmin = ra[0]
+    cxmax = ra[-1]
+    cymin = dec[-1]
+    cymax = dec[0]
+
+    # Set limits and tics of colorbar - velocity scale
+    print(np.min(im))
+    print(np.max(im))
+
+    # Plot image as a colour map in units of micro Jy
+    im = im * 1e6
+    im[257, 254]
+    cmap = ax.imshow(im,
+                     extent=[cxmin, cxmax, cymin, cymax],
+                     vmin=np.min(im),
+                     vmax=np.max(im),
+                     cmap=cpal)
+
+    # Scale countour levels to micro Jy and plot contours
+    cont_levs = cont_levs * rms * 1e6
+
+    if residuals:
+        ax.contour(resid,
+                   levels=cont_levs,
+                   colors='k',
+                   linewidths=0.75,
+                   linestyles='solid')
+        ax.contour(resid,
+                   levels=-1 * np.flip(cont_levs, axis=0),
+                   colors='k',
+                   linewidths=0.75,
+                   linestyles='dashed')
+    else:
+        ax.contour(ra, dec, im,
+                   colors='k',
+                   levels=cont_levs,
+                   linewidths=0.75,
+                   linestyles='solid')
+        ax.contour(ra, dec, im,
+                   levels=-1 * np.flip(cont_levs, axis=0),
+                   colors='k',
+                   linewidths=0.75,
+                   linestyles='dashed')
+
+    # Create the colorbar
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("top", size="8%", pad=0.0)
+    cbar = fig.colorbar(cmap, ax=ax, cax=cax, orientation='horizontal')
+    cbar.ax.xaxis.set_tick_params(direction='out',
+                                  length=3,
+                                  which='major',
+                                  bottom='off',
+                                  top='on',
+                                  labelsize=8,
+                                  pad=-2,
+                                  labeltop='on',
+                                  labelbottom='off')
+    cbar.ax.xaxis.set_tick_params(direction='out',
+                                  length=2,
+                                  which='minor',
+                                  bottom='off',
+                                  top='on')
+    minorLocator = AutoMinorLocator(cbtmj / cbtmn)
+    cbar.ax.xaxis.set_minor_locator(minorLocator)
+    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(),
+                            rotation=45, fontsize=11)
+    cbar.set_ticks(np.arange(cbmin, cbmax, cbtmj))
+
+    # Colorbar label
+    cbar.ax.text(0.432, 0.365, r'$\mu Jy / bm$', fontsize=13,
+                 path_effects=[PathEffects.withStroke(linewidth=2, foreground="w")])
+
+    # Overplot the beam ellipse
+    beam_ellipse_color = 'k'
+    bmin = head['bmin'] * 3600.
+    bmaj = head['bmaj'] * 3600.
+    bpa = head['bpa']
+    el = Ellipse(xy=[-4.3, -4.3],
+                 width=bmin,
+                 height=bmaj,
+                 angle=-bpa,
+                 edgecolor='k',
+                 hatch='/////',
+                 facecolor='none',
+                 zorder=10)
+    ax.add_artist(el)
+
+    # Plot the scale bar
+    x = 3.97
+    y = -4.7
+    ax.plot([x, x - 1], [y, y], '-', linewidth=2, color='k')
+    ax.text(x + 0.2, y + 0.15, "10 au", fontsize=17,
+            path_effects=[PathEffects.withStroke(linewidth=2, foreground="w")])
+
+    # Plot a cross at the source position
+    ax.plot([0.0], [0.0], '*', markersize=9, markeredgewidth=1, color='k')
+
+    # Add figure text
+    if text:
+        for t in text:
+            ax.text(*t, fontsize=18,
+                    path_effects=[PathEffects.withStroke(linewidth=3, foreground="w")])
+
+
+# Set seaborn plot styles
+sns.set_style("ticks",
+              {"xtick.direction": "in",
+               "ytick.direction": "in"})
+sns.set_context("talk")
+
+
+# Set colorpalette
+from my_colormaps import *
+cpals = [cubehelix_1, cubehelix_2, cubehelix_3, jesse_reds]
+colorsaves = ['cubehelix_1', 'cubehelix_2', 'cubehelix_3', 'jesse_reds']
+which_cpal = 3
+
+# Create figure
+fig, (ax1, ax2) = plt.subplots(
+    1, 2, sharex=False, sharey=False, figsize=(11.6, 6.2))
+
+# Plot subplots on seperate axes
+return_axis(ax=ax1,
+            image='aumic_marjune_concat_usermask_natural',
+            axislabels=True,
+            cpal=cpals[which_cpal],
+            cbmin=-50,
+            cbmax=301,
+            cbtmj=100,
+            cbtmn=20,
+            rms=1.77061756403e-05,
+            cont_levs=np.arange(2, 40, 2),
+            text=[(4.7, 4.3, 'AU Mic ALMA 1.4mm'),
+                  (3.65, 3.7, r'natural weighting')])
+
+return_axis(ax=ax2,
+            image='aumic_marjune_concat_usermask_200klam',
+            axislabels=False,
+            cpal=cpals[which_cpal],
+            cbmin=-50,
+            cbmax=601,
+            cbtmj=50,
+            cbtmn=10,
+            rms=2.19239682338e-05,
+            cont_levs=np.arange(2, 40, 2),
+            text=[(4.7, 4.3, 'AU Mi c ALMA 1.4mm'),
+                  (4.15, 3.7, r'200 k$\lambda$ taper')])
+
+
+plt.subplots_adjust(wspace=0)
+
+# Save and show figure
+plt.savefig('AUmic_marjune_concat_naturaltaper.png')
+plt.show()
