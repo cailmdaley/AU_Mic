a5528eca9f3f09ca5fc23f8926f57bcdb48e9eb2
cailmdaley <cailmdaley@gmail.com>
2017-03-31 20:53:11 -0400

    added 257 pixel image

--- Imaging/cleans/imaging_script.py
+++ Imaging/cleans/imaging_script.py
@@ -11,9 +11,10 @@ mask = '../aumic_larger.mask'
 # Concat before clean to fix proper motion offset
 files = files[4:12]
 print(files)
-concat_name = "aumic_junmar"
-subprocess.call("rm -rf {}".format(concat_name + "_concat.ms"), shell=True)
-concat(vis=files, concatvis=(concat_name + "_concat.ms"), dirtol='2arcsec' )
+concat_name = "aumic_ctrpix_test"
+vis_name = 'aumic_composite'
+# subprocess.call("rm -rf {}".format(concat_name + "_concat.ms"), shell=True)
+# concat(vis=files, concatvis=(concat_name + "_concat.ms"), dirtol='2arcsec' )
 
 #==========================================================
 #rms values for various cleans
@@ -29,9 +30,9 @@ mar_200klam_rms = 2.84779671347e-05
 # Natural no taper: residual is the dirty image
 image = concat_name+"_dirty_natural"
 subprocess.call("rm -rf {}.*".format(image), shell=True)
-tclean(vis=concat_name + "_concat.ms",
+tclean(vis= vis_name + "_concat.ms",
        imagename=image,
-       imsize=512,
+       imsize=513,
        cell='0.03arcsec',
        weighting='natural',
        niter=0)
@@ -41,9 +42,9 @@ rms = imstat(imagename='{}.residual'.format(image),
 # User mask:
 image = concat_name + "_usermask_natural"
 subprocess.call("rm -rf {}.*".format(image), shell=True)
-tclean(vis=concat_name + "_concat.ms",
+tclean(vis=vis_name + "_concat.ms",
        imagename=image,
-       imsize=512,
+       imsize=513,
        cell='0.03arcsec',
        weighting='natural',
        niter=100000,
@@ -61,38 +62,38 @@ exportfits(imagename='{}.image'.format(image),
            fitsimage='{}.fits'.format(image))
 
 
-# Natural 200klambda taper: residual is the dirty image
-image = concat_name + "_dirty_200klam"
-subprocess.call("rm -rf {}.*".format(image), shell=True)
-tclean(vis=concat_name + "_concat.ms",
-       imagename=image,
-       imsize=512,
-       cell='0.03arcsec',
-       weighting='natural',
-       uvtaper=['200klambda'],
-       niter=0)
-rms = imstat(imagename='{}.residual'.format(image),
-             region='../rms.region', listit=False)['rms'][0]
-
-# User mask:
-image = concat_name + "_usermask_200klam"
-subprocess.call("rm -rf {}.*".format(image), shell=True)
-tclean(vis=concat_name + "_concat.ms",
-       imagename=image,
-       imsize=512,
-       cell='0.03arcsec',
-       weighting='natural',
-       uvtaper=['200klambda'],
-       niter=100000,
-       threshold=rms / 2.,
-       usemask='user',
-       mask=mask,
-       pbmask=None)
-viewer(infile=image + '.image')
-rms = imstat(imagename='{}.image'.format(image), region='../rms.region',
-             listit=False)['rms'][0]
-print(rms)
-
-# Export to .fits
-exportfits(imagename='{}.image'.format(image),
-           fitsimage='{}.fits'.format(image))
+# # Natural 200klambda taper: residual is the dirty image
+# image = concat_name + "_dirty_200klam"
+# subprocess.call("rm -rf {}.*".format(image), shell=True)
+# tclean(vis=concat_name + "_concat.ms",
+#        imagename=image,
+#        imsize=512,
+#        cell='0.03arcsec',
+#        weighting='natural',
+#        uvtaper=['200klambda'],
+#        niter=0)
+# rms = imstat(imagename='{}.residual'.format(image),
+#              region='../rms.region', listit=False)['rms'][0]
+#
+# # User mask:
+# image = concat_name + "_usermask_200klam"
+# subprocess.call("rm -rf {}.*".format(image), shell=True)
+# tclean(vis=concat_name + "_concat.ms",
+#        imagename=image,
+#        imsize=512,
+#        cell='0.03arcsec',
+#        weighting='natural',
+#        uvtaper=['200klambda'],
+#        niter=100000,
+#        threshold=rms / 2.,
+#        usemask='user',
+#        mask=mask,
+#        pbmask=None)
+# viewer(infile=image + '.image')
+# rms = imstat(imagename='{}.image'.format(image), region='../rms.region',
+#              listit=False)['rms'][0]
+# print(rms)
+#
+# # Export to .fits
+# exportfits(imagename='{}.image'.format(image),
+#            fitsimage='{}.fits'.format(image))
