074d55edbc83a066c1717a533d0b807040736bc8
cailmdaley <cailmdaley@gmail.com>
2017-05-01 02:14:42 -0400

    recleaned everything to 2 sigma in dirty map
    Former-commit-id: 2402d4eb433d4ef59fd6102d3feaa696288273e5

--- Imaging/cleans/imaging_script.py
+++ Imaging/cleans/imaging_script.py
@@ -11,38 +11,42 @@ from glob import glob
 dates = ['aumic_aug.concat.ms',
          'aumic_jun_timing.concat.ms',
          'aumic_mar.concat.ms']
-concat_files = [dates[1], dates[2]]
+concat_files = ['../../data_files/24jun2015_aumic1_spw1.timing.reweighted.ms']
 print 'Files to be cleaned are:', concat_files
 raw_input('ok?: ')
 # concat_files.append('../Visibility_processing/24jun2015_aumic1_spw3.timeflag.reweighted.ms')
 
-filename = 'aumic_marjun'
+filename = 'aumic_jun_timing_spw1'
 imsize = 512
 pixsize ='0.03arcsec',
 
-natural = True
-natural_mask = 'aumic_marjun_long_bsl_mask.region'
 
-taper = True
+natural = True
+natural_mask = 'aumic_jun_mask.region'
+
+taper = False
 taper_mask = 'aumic_marjun_short_bsl_mask.region'
+
+view=True
 
 # Concat before clean to fix proper motion offset;
 #pointing center of first chronological date is used
-if len(concat_files) > 1:
-    print('Concatenating...')
-    subprocess.call("rm -rf {}".format(filename + ".concat.ms"), shell=True)
-    concat(vis=concat_files, concatvis=(filename + ".concat.ms"), dirtol='2arcsec' )
+# if len(concat_files) > 1:
+print('Concatenating...')
+subprocess.call("rm -rf {}".format(filename + ".concat.ms"), shell=True)
+concat(vis=concat_files, concatvis=(filename + ".concat.ms"), dirtol='2arcsec' )
 
 #==========================================================
 #rms values for various cleans
-aug_natural_rms = 3.02602056763e-05
-jun_timing_rms = 2.09834179259e-05
-augjun_natural_rms = 1.86735014722e-05
-mar_200klam_rms = 2.94901055895e-05
-composite_natural_rms = 1.51412368723e-05
-composite_200klam_rms = 1.91825256479e-05
-marjun_natural_rms = 1.66271911439e-05
-marjun_200klam_rms = 2.13612966036e-05
+aug_natural_rms = 3.83226033591e-05
+jun_timing_rms = 2.79580763163e-05
+jun_timing_spw1_rms = 4.26983060606e-05
+mar_200klam_rms = 2.92891236313e-05
+augjun_natural_rms = 2.25834228331e-05
+composite_natural_rms = 1.79467988346e-05
+composite_200klam_rms = 2.14701703953e-05
+marjun_natural_rms = 2.02876035473e-05
+marjun_200klam_rms = 2.39355267695e-05
 #==========================================================
 
 
@@ -57,8 +61,11 @@ if natural:
            cell=pixsize,
            weighting='natural',
            niter=0)
-    rms = imstat(imagename='{}.residual'.format(image),
+    dirty_natural_rms = imstat(imagename='{}.residual'.format(image),
                  region='aumic_rms.region', listit=False)['rms'][0]
+    if view:
+        viewer(infile=image + '.residual')
+        print 'dirty natural: {}'.format(dirty_natural_rms)
 
     # User mask:
     image = filename + "_natural"
@@ -69,11 +76,12 @@ if natural:
            cell=pixsize,
            weighting='natural',
            niter=100000,
-           threshold=rms / 2.,
+           threshold=dirty_natural_rms / 2.,
            usemask='user',
-           mask=mask,
+           mask=natural_mask,
            pbmask=None)
-    viewer(infile=image + '.image')
+    if view:
+        viewer(infile=image + '.image')
     natural_rms = imstat(imagename='{}.image'.format(image),
                  region='aumic_rms.region', listit=False)['rms'][0]
     print 'Natural clean rms is', natural_rms
@@ -93,8 +101,11 @@ if taper:
           weighting='natural',
           uvtaper=['200klambda'],
           niter=0)
-    rms = imstat(imagename='{}.residual'.format(image),
+    dirty_200klam_rms = imstat(imagename='{}.residual'.format(image),
                 region='aumic_rms.region', listit=False)['rms'][0]
+    if view:
+        viewer(infile=image + '.residual')
+        print 'dirty taper: {}'.format(dirty_natural_rms)
 
     # User mask:
     image = filename + "_200klam"
@@ -106,11 +117,12 @@ if taper:
           weighting='natural',
           uvtaper=['200klambda'],
           niter=100000,
-          threshold=rms / 2.,
+          threshold=dirty_200klam_rms / 2.,
           usemask='user',
-          mask=mask,
+          mask=taper_mask,
           pbmask=None)
-    viewer(infile=image + '.image')
+    if view:
+        viewer(infile=image + '.image')
     taper_rms = imstat(imagename='{}.image'.format(image), region='aumic_rms.region',
                 listit=False)['rms'][0]
     print 'Taper clean rms is', taper_rms
