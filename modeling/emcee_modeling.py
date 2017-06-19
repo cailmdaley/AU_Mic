eebf0adc5e93fab47dd08c5ed3aaf38ecc4e0314
Cail Daley <cailmdaley@gmail.com>
2017-06-19 14:16:50 -0400

    changed the notes some
    Former-commit-id: d6c5a48c2cfb09d40980c4c6c4be0cd4a878b2f2

--- modeling/emcee_modeling.py
+++ modeling/emcee_modeling.py
@@ -1,117 +1,157 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
-import subprocess
+import subprocess as sp
 import emcee
 
+    
 class Observation:
     def __init__(self, name):
         self.name = name
-        self.fits = fits.open(name + '.fits')
-        self.uvf  = fits.open(name + '.uvf')
-        # self.visname  = fits.open(name + '.uvf')
+        self.im = fits.open('obs_data/{}.fits'.format(name))
+        self.uvf  = fits.open('obs_data/{}.uvf'.format(name))
 
-        self.ra = self.fits[0].header['CRVAL1']
-        self.dec = self.fits[0].header['CRVAL2']
+        self.ra = self.im[0].header['CRVAL1']
+        self.dec = self.im[0].header['CRVAL2']
 
 class Model:
-    def __init__(self, name):
+    
+    def obs_sample(self, obs):
+        """
+        Create model fits file with correct header information, remove stellar emission, convolve with ALMA visiblities to create model .vis and .uvf files.
+        """
+        
+        filename = self.name + '_' + obs.name
+
+        # first, delete all model files with same name
+        sp.call('rm -rf model_data/{}*'.format(filename), shell=True)
+        
+        # align with observation
+        self.im[0].header['CRVAL1'] = obs.ra
+        self.im[0].header['CRVAL2'] = obs.dec
+
+        self.im.writeto('model_data/{}.fits'.format(filename))
+
+        # Convert model into MIRIAD .im
+        sp.call(['fits', 'op=xyin', 
+            'in=model_data/{}.fits'.format(filename),
+            'out=model_data/{}.im'.format(filename)])
+
+        # Sample the model image using the observation uv coverage
+        sp.call(['uvmodel', 'options=replace',
+            'vis=obs_data/{}.vis'.format(obs.name), 
+            'model=model_data/{}.im'.format(filename),
+            'out=model_data/{}.vis'.format(filename)])
+            
+        #Convert to UVfits
+        print(filename)
+        sp.call(['fits', 'op=uvout', 
+            'in=model_data/{}.vis'.format(filename),
+            'out=model_data/{}.uvf'.format(filename)])
+            
+            
+    def get_chi(self, obs):
+        """
+        Return chi^2 of model.
+        """
+
+        datrlimwt = obs.uvf[0].data['data']
+        datrlxx = datrlimwt[:, 0, 0, 0, 0, 0, 0]
+        datrlyy = datrlimwt[:, 0, 0, 0, 0, 1, 0]
+        datimxx = datrlimwt[:, 0, 0, 0, 0, 0, 1]
+        datimyy = datrlimwt[:, 0, 0, 0, 0, 1, 1]
+        weights = datrlimwt[:, 0, 0, 0, 0, 0, 2]
+        datrlI = np.array((datrlxx + datrlyy) / 2.)
+        datimI = np.array((datimxx + datimyy) / 2.)
+
+        self.uvf  = fits.open('model_data/{}_{}.uvf'.format(self.name, obs.name))
+        modrlimwt = self.uvf[0].data['data']
+        modrlI = modrlimwt[::2, 0, 0, 0, 0, 0]
+        modimI = modrlimwt[::2, 0, 0, 0, 0, 1]
+        self.uvf.close
+
+        # Calculate chi^2
+        chi = np.sum((datrlI - modrlI)**2 * weights +
+                     (datimI - modimI)**2 * weights)
+        redchi = chi / len(datrlI)
+
+        return redchi
+            
+    def clean(self, obs, residual=False, show=True):
+        """
+        Clean and image a miriad visibility file; uses imstat to print rms, and then asks the user to input a clean cutoff level.
+        """
+        filename = self.name + '_' + obs.name
+        if residual == True:
+            filename += '.residual'
+
+        sp.call('rm -rf model_data/{}.{{mp,bm,cl,cm}}'.format(filename), shell=True)
+        sp.call(['invert', 
+            'vis=model_data/{}.vis'.format(filename), 
+            'map=model_data/{}.mp'.format(filename), 
+            'beam=model_data/{}.bm'.format(filename), 
+            'cell=0.03arcsec', 'imsize=512', 'options=systemp,mfs', 'robust=2'])
+            
+        sp.call(['imstat', 
+            'in=model_data/{}.mp'.format(filename), 
+            "region='boxes(256,0,512,200)'"])
+        cutoff = input('Please enter CLEAN cutoff:  ')
+        # cutoff = 6.657E-12
+        
+        sp.call(['clean', 
+            'map=model_data/{}.mp'.format(filename), 
+            'beam=model_data/{}.bm'.format(filename), 
+            'out=model_data/{}.cl'.format(filename), 
+            'niters=10000', 'cutoff={}'.format(cutoff)])
+             
+        sp.call(['restor',
+            'map=model_data/{}.mp'.format(filename),
+            'beam=model_data/{}.bm'.format(filename),
+            'model=model_data/{}.cl'.format(filename),
+            'out=model_data/{}.cm'.format(filename)])
+        
+        if show == True:
+            
+            sp.call(['imstat', 
+                'in=model_data/{}.cm'.format(filename), 
+                "region='boxes(256,0,512,200)'"])
+            rms = input('Please enter CLEAN cutoff:  ')
+            
+            sp.call(['cgdisp', 
+                'in=model_data/{}.cm,model_data/{}.cm'.format(filename, filename), 
+                'type=p,c', 'device=/xs', 
+                'slev=a,{}'.format(rms), 'levs1=-6,-4,-2,2,4,6',
+                'region=arcsec,box(-5,-5,5,5)',
+                'labtyp=arcsec', 'beamtyp=b,l,3',])
+
+        
+    def residuals(self, obs, show=True):
+        filename = self.name + '_' + obs.name
+        
+        sp.call(['uvmodel', 'options=subtract',
+            'model=model_data/{}.im'.format(filename),
+            'vis=obs_data/{}.vis'.format(obs.name),
+            'out=model_data/{}.residual.vis'.format(filename)])
+    
+        if show == True:
+            self.clean(obs, residual=True)
+        
+            
+    def __init__(self, path, observations, name='model'):
+        self.path = path
         self.name = name
-        self.fits = fits.open(name + '.fits')
+        self.im = fits.open(path + '.fits')
+        
+        self.observations = observations
+        
+        self.redchis = []
+        for obs in observations:
+            self.obs_sample(obs)
+            self.redchis.append(self.get_chi(obs))
+        self.im.close
+        
 
-def chi(model_data):
-    return (obs_data - model_data)**2 / rms
-
-
-def model_convolve(model, obs):
-
-    """
-    Create model fits file with correct header information, remove stellar emission, convolve with ALMA visiblities to create model .vis and .uvf files.
-    """
-
-    # subprocess.call('mkdir model_files', shell=True)
-
-    #Read in model and observations
-    # model = fits.open('{}.fits'.format(modelname))[0]
-
-    #If model lacks a pointing center, align with observation
-    if model.fits[0].header['CRVAL1'] == 0:
-        model.fits[0].header['CRVAL1'] = obs.ra
-        model.fits[0].header['CRVAL2'] = obs.dec
-
-        model.fits.writeto('{}.fits'.format(model.name), clobber=True)
-        model.fits.close
-
-    subprocess.call('rm -r {}.im'.format(model.name), shell=True)
-    subprocess.call(['fits', 'in={}.fits'.format(model.name),
-                     'op=xyin', 'out={}.im'.format(model.name)])
-
-    subprocess.call('rm -r {}.vis'.format(model.name), shell=True)
-    subprocess.call(['uvmodel', 'model={}.im'.format(model.name),
-        'vis={}.vis'.format(obs.name), 'options=replace',
-        'out={}.vis'.format(model.name)])
-    subprocess.call(['fits', 'in={}.vis'.format(model.name),
-        'op=uvout', 'out={}.uvf'.format(model.name)])
-
-def get_chi(obs, model):
-    """
-    Return chi^2 of model.
-    """
-
-    datrlimwt = obs.uvf[0].data['data']
-
-    # splitting visibilities by time removes one index from datrlimwt array:
-    # so, must check size of datrlimwt
-    datrlxx = datrlimwt[:, 0, 0, 0, 0, 0, 0]
-    datrlyy = datrlimwt[:, 0, 0, 0, 0, 1, 0]
-    datimxx = datrlimwt[:, 0, 0, 0, 0, 0, 1]
-    datimyy = datrlimwt[:, 0, 0, 0, 0, 1, 1]
-    weights = datrlimwt[:, 0, 0, 0, 0, 0, 2]
-    # elif len(datrlimwt.shape) == 6:
-    #     datrlxx = datrlimwt[:, 0, 0, 0, 0, 0]
-    #     datrlyy = datrlimwt[:, 0, 0, 0, 1, 0]
-    #     datimxx = datrlimwt[:, 0, 0, 0, 0, 1]
-    #     datimyy = datrlimwt[:, 0, 0, 0, 1, 1]
-    #     weights = datrlimwt[:, 0, 0, 0, 0, 2]
-    datrlI = np.array((datrlxx + datrlyy) / 2.)
-    datimI = np.array((datimxx + datimyy) / 2.)
-
-    model.uvf  = fits.open(model.name + '.uvf')
-    modrlimwt = model.uvf[0].data['data']
-    modrlI = modrlimwt[::2, 0, 0, 0, 0, 0]
-    modimI = modrlimwt[::2, 0, 0, 0, 0, 1]
-
-    # Calculate chi^2
-    chi = np.sum((datrlI - modrlI)**2 * weights +
-                 (datimI - modimI)**2 * weights)
-    redchi = chi / len(datrlI)
-
-    print chi, redchi
-    # obs.close
-    # model.close
-
-def image_vis(model, show=True):
-    """
-    Clean and image a miriad visibility file; uses imstat to print rms, and then asks the user to input a clean cutoff level.
-    """
-
-    pixsize = '0.03arcsec'
-    subprocess.call(['rm -r ' + model.name + '.{mp,bm,cl,cm}'], shell=True)
-    subprocess.call('invert vis={}.vis map={}.mp beam={}.bm cell={} imsize=512 options=systemp,mfs robust=2'.format(
-        model.name, model.name, model.name, pixsize), shell=True)
-    subprocess.call(['imstat', 'in={}.mp'.format(model.name),
-                     "region='boxes(256,0,512,200)'"])
-    cutoff = input('Please enter CLEAN cutoff:  ')
-    subprocess.call('clean map={}.mp beam={}.bm out={}.cl niters=10000 cutoff={}'.format(
-        model.name, model.name, model.name, cutoff), shell=True)  # cutoff=6.290E-05
-    subprocess.call('restor map={}.mp beam={}.bm model={}.cl out={}.cm'.format(
-        model.name, model.name, model.name, model.name), shell=True)
-    if show == True:
-        subprocess.call(['cgdisp', 'in={}.cm'.format(
-            model.name), 'device=/xs', 'labtyp=arcsec', 'beamtyp=b,l,3'])
-
-
-aug = Observation('obs_data/aumic_aug')
-model = Model('model_data/first_model')
-model_convolve(model, aug)
-chi = get_chi(aug, model)
+aug = Observation('aug')
+jun = Observation('jun')
+first_model = Model('first_model', [aug,jun])
+# chi = get_chi(aug, model)
