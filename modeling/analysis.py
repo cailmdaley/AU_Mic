c770c1fe121d57cfef9e0be94a610dd3c54d9a7c
Cail Daley <cailmdaley@gmail.com>
2017-07-14 02:38:21 -0400

    actually wrote the walker plot function in this commit

--- modeling/analysis.py
+++ modeling/analysis.py
@@ -6,42 +6,64 @@ import numpy as np
 sns.set_style("ticks")
 sns.set_context("talk")
 
-def pairplot(run_name):
+def walker_evolution(run_name, nwalkers):
+    plt.close()
+    run_name = 'run3'
+    posterior = pd.read_csv(run_name + '.csv')
+    posterior.index /= nwalkers
+    
+    # posterior.reset_index(inplace=True)
+    # posterior.rename(columns={'index' : 'walker'}, inplace=True)
+    # posterior
+    # posterior['walker'] %= nwalkers
+    
+    axes = posterior.iloc[0::nwalkers].plot(subplots=True, color='black', alpha=0.5)
+    for i in range(nwalkers):
+        posterior.iloc[i+1::nwalkers].plot(subplots=True, ax=axes, legend=False, color='black', alpha=0.5)
+        
+    #quickly find misbehaving walkers
+    # posterior.iloc[7::nwalkers].plot(subplots=True, ax=axes, color='red', alpha=0.5, legend=False)
+    
+    plt.show()
+         
+def corner(run_name, nwalkers, burn_in=0, bad_walkers=[]):
     """ Plot 'corner plot' of fit"""
     plt.close()
     
-    # run_name = 'run2-priors'
-    # run_name = 'run1'
-    posterior = pd.read_csv(run_name + '.csv')
-    posterior['iteration'] = pd.cut(posterior.index.values, 2)
-    # posterior.ix[1000:,'burn_in'] = 0
+    # run_name = 'run3'
+    samples = pd.read_csv(run_name + '.csv' )
+    posterior = samples.iloc[burn_in*nwalkers:]
+        
+    posterior = posterior.drop([row for row in posterior.index if row%nwalkers in bad_walkers])
+    posterior=posterior.drop('lnprob', axis=1)
+    posterior.rename(columns={'m_disk' : '$\log$ m_disk'}, inplace=True)
+    print(posterior.mean())
 
-    
-    # try:
-    #     posterior['m_disk'] = 3.67 * 10**posterior['m_disk']
-    # except KeyError:
-    #     pass
-
-    # cmap = sns.cubehelix_palette(as_cmap=True, start=2.3, dark=0, light=1, reverse=True)
     cmap = "Blues"
-    corner = sns.PairGrid(posterior, hue='iteration', diag_sharey=False, despine=False)
+    corner = sns.PairGrid(posterior, diag_sharey=False, despine=False)
     corner.map_diag(sns.kdeplot, cut=0)
-    corner.map_lower(sns.kdeplot, cut=0, cmap=cmap, n_levels=10, shade=True)
-    corner.map_upper(plt.scatter)
-    corner.add_legend()
+    corner.map_lower(sns.kdeplot, cut=0, cmap=cmap, n_levels=3, shade=True)
+    corner.map_lower(plt.scatter, s=1, color='#708090', alpha=0.2)
+    corner.map_lower(sns.kdeplot, cut=0, cmap=cmap, n_levels=3, shade=False)
+    
+    for i, j in zip(*np.triu_indices_from(corner.axes, 1)):
+        corner.axes[i, j].set_visible(False)
+    
     for ax in corner.axes.flat:
         ax.xaxis.set_major_formatter(FormatStrFormatter('%.2g'))
         ax.yaxis.set_major_formatter(FormatStrFormatter('%.2g'))
     plt.subplots_adjust(top=0.95)
     corner.fig.suptitle("{}".format(run_name))
     
-    corner.savefig('{}.pairgrid.png'.format(run_name)); 
+    corner.savefig('{}.pairgrid.png'.format(run_name), dpi=700); 
     plt.show(False)
     
     # posterior.hist(bins=20)
     # plt.savefig('{}.hist.png'.format(run_name)); plt.show()
     
     return posterior
-
-test=pairplot('run2-priors')
+    
+posterior.mean()
+test=corner('run3', nwalkers=8, burn_in=50, bad_walkers=[7])
+# walker_evolution('run3', nwalkers=8)
 # test=pairplot('run1')
