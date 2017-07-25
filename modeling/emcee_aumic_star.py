85975aa60614da215567657c89d59b0f66a8475d
Cail Daley <cailmdaley@gmail.com>
2017-07-25 13:16:47 -0400

    mac: getting ready for conflict~
    Former-commit-id: e6103896a6f432c779dbfa4f235250a56dcd00e4

--- modeling/emcee_aumic_star.py
+++ modeling/emcee_aumic_star.py
@@ -1,17 +1,44 @@
+from aumic_obs_and_model import *
 from emcee.utils import MPIPool
 
-            
-            
 # define likelehood functions
-def lnlike(theta, modelname, to_vary):
-    
+# def lnprob(theta, modelname, to_vary):
+#     """
+#     For each parameter to be varied, return -infinity if the proposed value lies
+#     outside the bounds for the parameter; if all parameters are ok, return 0.
+#     """
+#     
+#     # reassign parameters to vary to those input by emcee
+#     for i, free_param in enumerate(to_vary):
+#         lower_bound, upper_bound = free_param[-1]
+#         print(free_param)
+#         print(theta[i])
+#         
+#         if lower_bound < theta[i] < upper_bound:
+#             params[free_param[0]] = theta[i]
+#         else: return -np.inf
+#         
+#     #logarithmic sampling
+#     params['m_disk'] = 10**params['m_disk'] 
+#     # since (k)evan's code takes r_out rather than d_r, add r_in to d_r to get r_out
+#     params['d_r'] += params['r_in']
+#     
+#     # create model
+#     model = Model(params.values(), observations=band6_observations, name=modelname)
+#     
+#     # return chi^2
+#     print(-0.5 * sum(model.chis))
+#     return -0.5 * sum(model.chis)
+
+# define likelehood functions
+def lnlike(theta, to_vary, observations):
     # default parameter values
     params = OrderedDict([
         ('temp_index',        -0.5),
         ('m_disk',            3.67e-08),
         ('sb_law',            2.3),
         ('r_in',              8.8),
-        ('d_r',                31.5),
+        ('r_out',             40.3),
         ('r_crit',            150.0),
         ('inc',               89.5),
         ('m_star',            0.31),
@@ -26,41 +53,38 @@ def lnlike(theta, modelname, to_vary):
         ('l_star',            0.09),
         ('scale_factor',      0.1),
         ('pa',                128.41),
-        ('mar_starflux',      3.67e-4),
-        ('aug_starflux',      1.23e-4),
-        ('jun_starflux',      2.62e-4)])
+        ('mar_starflux',      0.000367),
+        ('mar_starflux',      0.000367),
+        ])
     
     # reassign parameters to vary to those input by emcee
-    for i, param in enumerate(to_vary):
-        params[param[0]] = theta[i]
+    for i in range(len(to_vary)):
+        params[to_vary[i][0]] = theta[i]
         
-    #logarithmic sampling
-    params['m_disk'] = 10**params['m_disk'] 
-    # since (k)evan's code takes r_out rather than d_r, add r_in to d_r to get r_out
-    params['d_r'] += params['r_in']
+    params['m_disk'] = 10**params['m_disk']
     
     # create model
-    model = Model(params.values(), observations=band6_observations, name=modelname)
+    model = Model(params.values(), observations=observations)
     
     # return chi^2
     return -0.5 * sum(model.chis)
+def lnprior(theta):
+    m_disk, sb_law, scale_factor, r_in, r_out, inc, pa = theta
     
-def lnprior(theta, to_vary):
-    """
-    For each parameter to be varied, return -infinity if the proposed value lies
-    outside the bounds for the parameter; if all parameters are ok, return 0.
-    """
-    for i, param in enumerate(to_vary):
-        lower_bound, upper_bound = param[-1]
-        if not lower_bound < theta[i] < upper_bound:
-            return -np.inf
-    return 0.0
-    
-def lnprob(theta, modelname, to_vary):
-        lp = lnprior(theta, to_vary)
+    if -11. < m_disk and \
+    -4. < sb_law < 10. and \
+    0. < scale_factor < 2. and  \
+    0. < r_in < r_out and \
+    inc <= 90: 
+        return 0.0
+    else:
+        return -np.inf
+def lnprob(theta, to_vary, observations):
+        lp = lnprior(theta)
         if not np.isfinite(lp):
             return -np.inf
-        return lp + lnlike(theta, modelname, to_vary)
+        return lp + lnlike(theta, to_vary, observations)
+    
     
 def run_mcmc(nsteps, nwalkers, run_name, to_vary):
     
@@ -87,6 +111,7 @@ def run_mcmc(nsteps, nwalkers, run_name, to_vary):
         pos, chisum, blob = result
         with open(run_name + '.csv', 'a') as f: 
             np.savetxt(f, [np.append(pos[i], chisum[i]) for i in range(nwalkers)], delimiter=',')
+        sp.call('rm -rf model_data/*', shell=True)
 
     pool.close()
     
