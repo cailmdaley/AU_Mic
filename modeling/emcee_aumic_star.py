f85060932fa13943bcdd71d2773fdc86891cfdf3
cailmdaley <cailmdaley@gmail.com>
2017-08-02 14:48:43 -0400

    sirius: started run7

--- modeling/emcee_aumic_star.py
+++ modeling/emcee_aumic_star.py
@@ -1,8 +1,73 @@
+from astrocail.fitting import *
+from disk_model import debris_disk, raytrace
 from emcee.utils import MPIPool
-from aumic_obs_and_model import *
+from collections import OrderedDict
+import pandas as pd
+import emcee
+
+#==============================================================================#
+# Create observations, default parameter dict
+#==============================================================================#
+mar0 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_mar_spw0_FINAL', rms=6.5e-05)
+mar1 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_mar_spw1_FINAL', rms=6.124e-05)
+mar2 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_mar_spw2_FINAL', rms=6.068e-05)
+mar3 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_mar_spw3_FINAL', rms=6.468e-05)
+aug0 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_aug_spw0_FINAL', rms=5.879e-05)
+aug1 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_aug_spw1_FINAL', rms=5.336e-05)
+aug2 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_aug_spw2_FINAL', rms=6.092e-05)
+aug3 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_aug_spw3_FINAL', rms=5.558e-05)
+jun0 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_jun_spw0_FINAL', rms=5.369e-05)
+jun1 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_jun_spw1_FINAL', rms=4.658e-05)
+jun2 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_jun_spw2_FINAL', rms=5.083e-05)
+jun3 = Observation(root='unsubtracted_obs_files/', name='aumic_band6_jun_spw3_FINAL', rms=5.559e-05)
+band6_observations=[[mar0, mar1, mar2, mar3],
+                    [aug0, aug1, aug2, aug3],
+                    [jun0, jun1, jun2, jun3]]
+
+params = OrderedDict([
+    ('temp_index',        -0.5),
+    ('m_disk',            3.67e-08),
+    ('sb_law',            2.3),
+    ('r_in',              8.8),
+    ('d_r',                31.5),
+    ('r_crit',            150.0),
+    ('inc',               89.5),
+    ('m_star',            0.31),
+    ('co_frac',           0.0001),
+    ('v_turb',            0.081),
+    ('Zq',                70.0),
+    ('column_densities', [0.79, 1000]),
+    ('abundance_bounds', [50, 500]),
+    ('hand',              -1),
+    ('rgrid_size',        500),
+    ('zgrid_size',        500),
+    ('l_star',            0.09),
+    ('scale_factor',      0.1),
+    ('pa',                128.41),
+    ('mar_starflux',      3.67e-4),
+    ('aug_starflux',      1.23e-4),
+    ('jun_starflux',      2.62e-4)])
+
+def make_fits(disk_params, model):
+    structure_params = disk_params[:-1]
+    PA = disk_params[-1]
+
+    model_disk = debris_disk.Disk(structure_params, obs=[300, 131, 300, 20])
+    raytrace.total_model(model_disk,
+        distance=9.91, # pc
+        imres=0.03, # arcsec/pix
+        xnpix=512, #image size in pixels
+        freq0=model.observations[0][0].uvf[0].header['CRVAL4']*1e-9, # obs frequeency
+        PA=PA,
+        offs=[0.0,0.0], # offset from image center
+        nchans=1, # continum
+        isgas=False, # continuum!
+        includeDust=True, #continuuum!!
+        extra=0.0, # ?
+        modfile = model.root + model.name)
 
 # define likelehood functions
-def lnprob(theta, modelname, to_vary):
+def lnprob(theta, run_number, to_vary):
     """
     For each parameter to be varied, return -infinity if the proposed value lies
     outside the bounds for the parameter; if all parameters are ok, return 0.
@@ -20,37 +85,45 @@ def lnprob(theta, modelname, to_vary):
     params['d_r'] += params['r_in']
     
     # create model and get chi^2
-    model = Model(params.values(), observations=band6_observations, name=modelname)
-    model.chis=[]
-    for date, starflux in zip(model.observations, model.starfluxes):
+    disk_params = params.values()[:-3]
+    star_fluxes = params.values()[-3:]
+    
+    model = Model(observations=band6_observations, 
+        root=run_number + '_model_files/', name=str(np.random.randint(1e10)))
+    make_fits(disk_params, model)
+    
+    for date, star_flux in zip(model.observations, star_fluxes):
         for obs in date:
-            model.obs_sample(obs, starflux)
+            model_fits = fits.open(model.root + model.name + '.fits')
+
+            # add starflux to central pixel
+            crpix = int(model_fits[0].header['CRPIX1'])
+            model_im = model_fits[0].data[0]
+            model_im[crpix, crpix] += star_flux
+
+            # align with observation
+            model_fits[0].header['CRVAL1'] = obs.ra
+            model_fits[0].header['CRVAL2'] = obs.dec
+
+            #save
+            model_fits.writeto(model.root + model.name + obs.name + '.fits')
+
+            model.obs_sample(obs)
             model.get_chi(obs)
+            
     return -0.5 * sum(model.chis)
     
 def run_mcmc(nsteps, nwalkers, run_name, to_vary):
-    
     pool = MPIPool()
     if not pool.is_master():
         pool.wait()
         sys.exit(0)
-    # run sampler chain
+        
+    # initiate sampler chain
+    run_number = run_name[:4]
     ndim = len(to_vary)
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(run_name[:4], to_vary), pool=pool)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(run_number, to_vary), pool=pool)
     
-    # try:
-    #     posterior = pd.read_csv(run_name + '.csv')
-    #     print('Resuming {} at step {}'.format(run_name, posterior.index[-1]//nwalkers))
-    #     pos = np.array(posterior.iloc[-nwalkers:, :-1])
-    #     with open(run_name + '.csv', 'a') as f:
-    #         f.write('\n')
-    # except IOError, IndexError:
-    #     print('Starting {}'.format(run_name))
-    #     with open(run_name + '.csv', 'w') as f:
-    #         np.savetxt(f, (np.append([param[0] for param in to_vary], 'lnprob\n'),), 
-    #             delimiter=',', fmt='%s')
-    #     pos = [[param[1] + param[2]*np.random.randn() for param in to_vary] 
-    #         for i in range(nwalkers)]
     try:
         samples = pd.read_csv(run_name + '.csv')
         print('Resuming {} at step {}'.format(run_name, samples.index[-1]//nwalkers))
@@ -58,7 +131,9 @@ def run_mcmc(nsteps, nwalkers, run_name, to_vary):
         with open(run_name + '.csv', 'a') as f:
             f.write('\n')
     except IOError:
+        sp.call(['mkdir', run_name + '_model_files'])
         print('Starting {}'.format(run_name))
+        
         with open(run_name + '.csv', 'w') as f:
             np.savetxt(f, (np.append([param[0] for param in to_vary], 'lnprob\n'),), 
                 delimiter=',', fmt='%s')
