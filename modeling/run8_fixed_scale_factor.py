33548b21eea1e168342c654b2ae461bfc8308b63
Cail Daley <cailmdaley@gmail.com>
2017-08-15 14:37:41 -0400

    laptop: preparing for merge
    Former-commit-id: 828501ebef2426a9a18bbea35b27cad09c6d5073

--- modeling/run8_fixed_scale_factor.py
+++ modeling/run8_fixed_scale_factor.py
@@ -8,6 +8,62 @@ from astrocail import fitting, plotting, mcmc
 from disk_model import debris_disk, raytrace
 from aumic_observations import band6_observations, band6_rms_values, band6_fits_images
 
+def main():
+    parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter, description= '''Python commands associated with emcee run8, which has 50 walkers and varies the following parameters:
+    1)  disk mass
+    2)  surface brightness power law exponent
+    3)  inner radius
+    4)  outer radius (really inner radius + dr)
+    5)  inclination
+    6)  position angle
+    7)  march starflux
+    8)  august starflux
+    9)  june starflux
+This run fixes the scale factor at 0.03, in order to investigate how sensitive our modeling is to the puffiness of the disk. If such a low scale factor yields significantly worse chi squareds and residuals, we can conclude that we are able to detect differences and scale factor and can thus measure it. However, if the chi squareds and residuals do not worsen very much, this indicates we do not have the sensitivity to measure the scale factor, and can only report an upper limit.''')
+    
+    parser.add_argument('-r', '--run', action='store_true', 
+        help='begin or resume eemcee run.')
+        
+    parser.add_argument('-a', '--analyze', action='store_true', 
+        help='analyze sampler chain, producing an evolution plot, corner plot, and image domain figure.')
+    parser.add_argument('-b', '--burn_in', default=0,
+        help='number of steps \'burn in\' steps to exclude')
+    parser.add_argument('-c', '--corner', action='store_true', 
+        help='generate corner plot')
+    parser.add_argument('-e', '--evolution', action='store_true', 
+        help='generate walker evolution plot.')
+    parser.add_argument('-kde', '--density', action='store_true', 
+        help='generate kernel density estimate (kde) of posterior distribution')
+    args=parser.parse_args()
+    
+    
+    if args.run:
+        mcmc.run_emcee(run_name='run8', nsteps=10000, nwalkers=50,
+            lnprob = lnprob, to_vary = [
+            ('m_disk',            -7.55,        0.05,      (-np.inf, np.inf)),
+            ('sb_law',            1.5,          1.5,       (-5.,     10.)),
+            ('r_in',              10,           10,        (0,       np.inf)),
+            ('d_r',               31.5,         10,        (0,       np.inf)),
+            ('inc',               89,           1,         (0,       90)),
+            ('pa',                128.48,       0.1,       (1,       360)),
+            ('mar_starflux',      3.94e-4,      5e-5,      (0,       np.inf)),
+            ('aug_starflux',      1.50e-4,      1e-5,      (0,       np.inf)),
+            ('jun_starflux',      2.30e-4,      1e-5,      (0,       np.inf))])
+    else:
+        run = mcmc.MCMCrun('run8', nwalkers=50, burn_in=args.burn_in)
+        
+    if args.analyze:
+        make_best_fits(run)
+        run.evolution()
+        run.kde()
+        run.corner()
+        
+    if args.evolution:
+        run.evolution()
+    if args.density:
+        run.kde()
+    if args.corner:
+        run.corner()
 
 # default parameter dict:
 param_dict = OrderedDict([
@@ -28,7 +84,7 @@ param_dict = OrderedDict([
     ('rgrid_size',        500),
     ('zgrid_size',        500),
     ('l_star',            0.09),
-    ('scale_factor',      0.03),
+    ('scale_factor',      0.001),
     ('pa',                128.41),
     ('mar_starflux',      2.50e-4),
     ('aug_starflux',      2.50e-4),
@@ -43,7 +99,7 @@ def make_fits(model, disk_params):
         distance=9.91, # pc
         imres=0.03, # arcsec/pix
         xnpix=512, #image size in pixels
-        freq0=model.observations[0][0].uvf[0].header['CRVAL4']*1e-9, # obs frequeency
+        freq0=model.observations[0][0].uvf[0].header['CRVAL4']*1e-9, # obs frequency
         PA=PA,
         offs=[0.0,0.0], # offset from image center
         nchans=1, # continum
@@ -52,7 +108,7 @@ def make_fits(model, disk_params):
         extra=0.0, # ?
         modfile = model.root + model.name)
 
-    
+
 def fix_fits(model, obs, starflux):
     # open fits and add starflux,
     model_fits = fits.open(model.path + '.fits')
@@ -63,37 +119,37 @@ def fix_fits(model, obs, starflux):
     except AttributeError:
         model.crpix_diskflux = model_im[crpix, crpix]
         model_im[crpix, crpix] += starflux
-    
-    
+
+
     model_fits[0].header['CRVAL1'] = obs.ra
     model_fits[0].header['CRVAL2'] = obs.dec
-    
+
     model_fits.writeto(model.path + '.fits', overwrite=True)
-        
+
 # define likelehood functions
 def lnprob(theta, run_name, to_vary):
     """
     For each parameter to be varied, return -infinity if the proposed value lies
     outside the bounds for the parameter; if all parameters are ok, return 0.
     """
-    
+
     # reassign parameters to vary to those input by emcee
     for i, free_param in enumerate(to_vary):
         lower_bound, upper_bound = free_param[-1]
-        
+
         if lower_bound < theta[i] < upper_bound:
             param_dict[free_param[0]] = theta[i]
         else: return -np.inf
 
     param_dict['m_disk'] = 10**param_dict['m_disk']
     param_dict['d_r'] += param_dict['r_in']
-    
+
     disk_params = param_dict.values()[:-3]
     starfluxes = param_dict.values()[-3:]
-    
-    # intialize model and make fits image 
+
+    # intialize model and make fits image
     model = fitting.Model(observations=band6_observations,
-        root=run_name + '/model_files/', 
+        root=run_name + '/model_files/',
         name='model' + str(np.random.randint(1e10)))
     make_fits(model, disk_params)
     for pointing, starflux in zip(model.observations, starfluxes):
@@ -101,35 +157,37 @@ def lnprob(theta, run_name, to_vary):
             fix_fits(model, obs, starflux)
             model.obs_sample(obs)
             model.get_chi(obs)
-        
+
+    model.delete()
+
     return -0.5 * sum(model.chis)
 
 
 def make_best_fits(run):
-    subset_df = run.chain[run.chain['r_in'] < 15]
-    # subset_df = run.chain
+    # subset_df = run.main[run.main['r_in'] < 15]
+    subset_df = run.main
     model_params = subset_df[subset_df['lnprob'] == subset_df['lnprob'].max()].drop_duplicates() # best fit
     print('Model parameters:')
     print(model_params.to_string())
     print('')
-        
+
     for param in model_params.columns[:-1]:
         param_dict[param] = model_params[param].values
-        
-        
+
+
     param_dict['m_disk'] = 10**param_dict['m_disk']
     param_dict['d_r'] += param_dict['r_in']
 
     disk_params = param_dict.values()[:-3]
     starfluxes = param_dict.values()[-3:]
-    
-    # intialize model and make fits image 
+
+    # intialize model and make fits image
     print('Making model...')
     model = fitting.Model(observations=band6_observations,
-        root=run.name + '/model_files/', 
+        root=run.name + '/model_files/',
         name=run.name + '_bestfit')
     make_fits(model, disk_params)
-    
+
     print('Sampling and cleaning...')
     visibilities = []
     residuals = []
@@ -137,75 +195,34 @@ def make_best_fits(run):
         ids = []
         for obs in pointing:
             fix_fits(model, obs, starflux)
-            
+
             ids.append('_' + obs.name[12:20])
             model.obs_sample(obs, ids[-1])
             model.make_residuals(obs, ids[-1])
-            
+
         # make model visibilities
         vis_files = ','.join([model.path+ident+'.vis' for ident in ids])
         visibilities.append('{}_{}'.format(model.path, obs.name[12:15]))
         sp.call(['uvcat', 'vis={}'.format(vis_files), 'out={}.vis'.format(visibilities[-1])], stdout=open(os.devnull, 'wb'))
         model.clean(visibilities[-1], rms, show=False)
-        
+
         # make residuals
         res_files = ','.join([model.path+ident+'.residuals.vis' for ident in ids])
         residuals.append('{}_{}.residuals'.format(model.path, obs.name[12:15]))
         sp.call(['uvcat', 'vis={}'.format(res_files), 'out={}.vis'.format(residuals[-1])], stdout=open(os.devnull, 'wb'))
         model.clean(residuals[-1], rms, show=False)
-        
+
     print('Making figure...')
     fig = plotting.Figure(
-        paths=[[band6_fits_images[i], visibilities[i]+'.fits', residuals[i]+'.fits'] 
+        paths=[[band6_fits_images[i], visibilities[i]+'.fits', residuals[i]+'.fits']
             for i in range(len(visibilities))],
         rmses=[3*[rms] for rms in band6_rms_values],
-        texts=[[[[4.6, 4.0, date]], [[4.6, 4.0, 'rms={}'.format(np.round(rms*1e6))]], None] 
+        texts=[[[[4.6, 4.0, date]], [[4.6, 4.0, 'rms={}'.format(np.round(rms*1e6))]], None]
             for date, rms in zip(['March', 'August', 'June'], band6_rms_values)],
-        savefile=run.name+'/run6_bestfit_small_r_in.pdf', title=r'Run 6 Best Fit Model & Residuals for $r_{in} < 15$')
-        # savefile=run.name+'/run6_bestfit_global.pdf', title=r'Run 6 Global Best Fit Model & Residuals')
-        
-        
-        
-    
-        
+        # savefile=run.name+'/run8_bestfit_small_r_in.pdf', title=r'Run 8 Best Fit Model & Residuals for $r_{in} < 15$')
+        savefile=run.name+'/run8_bestfit_global.pdf', title=r'Run 8 Global Best Fit Model & Residuals')
+
+
     
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter, description= '''Python commands associated with emcee run8, which has 50 walkers and varies the following parameters:
-    1)  disk mass
-    2)  surface brightness power law exponent
-    3)  inner radius
-    4)  outer radius (really inner radius + dr)
-    5)  inclination
-    6)  position angle
-    7)  march starflux
-    8)  august starflux
-    9)  june starflux
-This run fixes the scale factor at 0.03, in order to investigate how sensitive our modeling is to the puffiness of the disk. If such a low scale factor yields significantly worse chi squareds and residuals, we can conclude that we are able to detect differences and scale factor and can thus measure it. However, if the chi squareds and residuals do not worsen very much, this indicates we do not have the sensitivity to measure the scale factor, and can only report an upper limit.''')
-    
-    parser.add_argument('-r', '--run', action='store_true', 
-        help='begin or resume eemcee run.')
-    parser.add_argument('-a', '--analyze', action='store_true', 
-        help='analyze sampler chain, producing an evolution plot, corner plot, and image-domain figure.')
-    parser.add_argument('-e', '--evolution', action='store_true', 
-        help='generate walker evolution plot.')
-    args=parser.parse_args()
-    
-    if args.run:
-        mcmc.run_emcee(run_name='run8', nsteps=10000, nwalkers=50, 
-            lnprob = lnprob, to_vary = [
-            ('m_disk',            -7.55,        0.05,      (-np.inf, np.inf)),
-            ('sb_law',            1.5,          1.5,       (-5.,     10.)), 
-            ('r_in',              10,           10,        (0,       np.inf)),
-            ('d_r',               31.5,         10,        (0,       np.inf)),
-            ('inc',               89,           1,         (0,       90)),
-            ('pa',                128.48,       0.1,       (1,       360)),
-            ('mar_starflux',      3.94e-4,      5e-5,      (0,       np.inf)),
-            ('aug_starflux',      1.50e-4,      1e-5,      (0,       np.inf)),
-            ('jun_starflux',      2.30e-4,      1e-5,      (0,       np.inf))])
-    else:
-        run = mcmc.MCMCrun('run8', nwalkers=50)
-        
-    if args.analyze:
-        make_best_fits(run)
-    if args.evolution:
-        run.evolution()
+    main()
