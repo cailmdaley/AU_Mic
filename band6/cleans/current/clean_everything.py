laptop: - compare_runs.py : now reports chi^2 and reduced chi^2. also, fixed calculation of nvis - run26...py, run27...py : added new Plotter call in make_best_fit() - run27: remade a bunch of figures - figures : updated with run26 & 27 plots - journal_article_V2.tex : lots of edits

band6/cleans/current/clean_everything.py
@@ -48,8 +48,7 @@ all_cleans = [
             
             
 rms_strings = []
-for obs in all_cleans:
-    casa.obs_clean(*obs)
+casa.obs_clean(*all_cleans[-1])
     # rms_strings.append(obs[0] + '_rms = ' + raw_input('please enter clean rms: '))
-print(np.array(rms_strings))
+# print(np.array(rms_strings))
     

band6/plots/current/scale_height_comparison.py
@@ -15,20 +15,20 @@ rs = np.linspace(37.5,42.5, 4)
 HWHMs = rs * 0.1/2
 ax1.plot(rs, HWHMs,  ls='-', color=c[2],
     label='Schuppler et al. (2015), short wavelengths')
-ax1.annotate(s='1a', xy=(rs[-1]-1, HWHMs[-1]-0.03), color=c[2], fontsize=fontsize)
+ax1.annotate(s='1a', xy=(rs[-1]-0.3, HWHMs[-1]), color=c[2], fontsize=fontsize)
 
 ## Metchev, apparent
 r = 40
 # HWHMs = np.linspace(2.5,4.2,4)/2
 HWHM = 2 
-ax1.plot(r, HWHM,  ls=':', label='Metchev et al. (2005)', color=c[1])
-ax1.annotate(s='2a', xy=(rs[-1]-1, HWHMs[-1]+0.06), color=c[1], fontsize=fontsize)
+ax1.plot(r, HWHM,  ls=':', label='Metchev et al. (2005)', color=c[1], marker='o')
+ax1.annotate(s='2a', xy=(r+3.1, HWHM-0.21), color=c[1], fontsize=fontsize)
 
 ## Krist, apparent
-rs_in = np.linspace(1.5, 5, 4)*distance
+rs = np.linspace(1.5, 5, 4)*distance
 HWHMs = np.linspace(0.25/2, 0.35/2, 4)*distance
 ax1.plot(rs, HWHMs,  ls=':', label='Krist et al. (2005), apparent', color=c[3])
-ax1.annotate(s='3a', xy=(rs[-1]-1, HWHMs[-1]+0.05), color=c[3], fontsize=fontsize)
+ax1.annotate(s='3a', xy=(rs[-1], HWHMs[-1]+0.05), color=c[3], fontsize=fontsize)
 
 ## Schneider
 rs = np.linspace(10, 50, 4)
@@ -93,6 +93,6 @@ ax2.set_title('Millimeter')
 
 plt.subplots_adjust(wspace=0, bottom=0.18)
 
-# plt.savefig('scale_height_comparison.png', dpi=1000)
-# plt.savefig('../../../writing/figures/scale_height_comparison.png', dpi=1000)
+plt.savefig('scale_height_comparison.png', dpi=1000)
+plt.savefig('../../../writing/figures/scale_height_comparison.png', dpi=1000)
 plt.show()

modeling/compare_runs.py
@@ -5,12 +5,12 @@ import numpy as np
 from glob import glob
 
 # create df
-run_info = pd.DataFrame(columns=['samples', 'k',  'lnprob', 'AICc', 'AIC_sigma', 'd_BIC'], dtype=float)
+run_info = pd.DataFrame(columns=['samples', 'k', 'chi^2', 'red_chi^2', 'lnprob', 'AICc', 'AIC_sigma', 'd_BIC'], dtype=float)
 run_info.name = 'run'
 
-# get # of visibilities
+# get number of visibilities; factor of 2 comes from reals and imaginaries
 uvfs = glob('subtracted_obs_files/*.uvf')
-nvis = np.sum([fits.open(uvf)[0].data.shape[0] for uvf in uvfs])
+nvis = 2 * np.sum([fits.open(uvf)[0].data.shape[0] for uvf in uvfs])
 
 
 # get get run directories and names
@@ -33,6 +33,8 @@ for run, run_name in zip(runs, run_names):
 
     run_info.loc[run_name, 'samples'] = chain.shape[0]
     run_info.loc[run_name, 'k'] = chain.shape[1]
+    run_info.loc[run_name, 'chi^2'] = -2*chain.lnprob.max()
+    run_info.loc[run_name, 'red_chi^2'] = -2*chain.lnprob.max() / nvis
     run_info.loc[run_name, 'lnprob'] = chain.lnprob.max()
 
 # calculate AIC --> AICc

modeling/run27_gaia_fiducial_broader.py
@@ -185,14 +185,14 @@ def lnprob(theta, run_name, to_vary):
 
 
 def make_best_fits(run, concise=False):
-    subset_df = run.main[run.main['r_in'] < 15]
-    model_params = subset_df[subset_df['lnprob'] == subset_df['lnprob'].max()].drop_duplicates() # best fit
+    subset_df = run.main#[run.main['r_in'] < 15]
+    model_params = subset_df[subset_df['lnprob'] == subset_df['lnprob'].max()].iloc[0] # best fit
     print('Model parameters:')
     print(model_params.to_string())
     print('')
 
-    for param in model_params.columns[:-1]:
-        param_dict[param] = model_params[param].values
+    for param in model_params.index[:-1]:
+        param_dict[param] = model_params[param]
 
 
     param_dict['m_disk'] = 10**param_dict['m_disk']
@@ -200,7 +200,6 @@ def make_best_fits(run, concise=False):
 
     disk_params = param_dict.values()[:-3]
     starfluxes = param_dict.values()[-3:]
-    starfluxes = [0,0,0]
 
     # intialize model and make fits image
     print('Making model...')
@@ -244,35 +243,50 @@ def make_best_fits(run, concise=False):
 
     print('Making figure...')
     if concise:
-        fig = plotting.Figure(
-            layout=(1,3),
-            paths=[
+        plotter = plotting.Plotter(
+            paths=[[
                 aumic_fitting.band6_fits_images[-1],
                 paths[-1] + '.fits',
-                paths[-1] + '.residuals.fits'],
-            rmses=3*[aumic_fitting.band6_rms_values[-1]],
-            texts=[
-                [[4.6, 4.0, 'Data']],
-                [[4.6, 4.0, 'Model']],
-                [[4.6, 4.0, 'Residuals']]
-                ],
-            title=None, 
-            savefile=run.name+'/' + run.name + '_bestfit_concise.pdf',
-            show=True)
-    else:
-        fig = plotting.Figure(layout=(4,3),
-           paths=[[obs, path + '.fits', path + '.residuals.fits']
-               for obs, path in zip(aumic_fitting.band6_fits_images, paths)],
-           rmses=[3*[rms] for rms in aumic_fitting.band6_rms_values],
-           texts=[
-               [[[4.6, 4.0, date]],
-               [[4.6, 4.0, 'rms={}'.format(np.round(rms*1e6))]],
-               None]
-               for date, rms in zip(['March', 'August', 'June', 'All'],
-               aumic_fitting.band6_rms_values)
-               ],
-           title= run.name + r'Global Best Fit Model & Residuals',
-          savefile=run.name+'/' + run.name + '_bestfit_global.pdf')
+                paths[-1] + '.residuals.fits']],
+            rmses=[3*[aumic_fitting.band6_rms_values[-1]]],
+            global_colorscale=False)
+        plotter.plot_all()
+
+        # Subplot labels
+        for i, text in enumerate(['Data', 'Model', 'Residuals']):
+            plotter.add_text(subplot_index=i, x=4.6, y=4.0, text=text)
+
+        # Scale bar
+        x = -3.015; y = -4.7
+        plotter.axes[0].plot([x, x - 10/9.725], [y, y], ls='-', linewidth=2, color='k')
+        plotter.add_text(subplot_index=0, x=x+0.32, y=y+0.15, text="10 au")
+
+        # For residual subplot: plot disk PA/extent as dashed line 
+        # and stellar location as star
+        proj_angle = np.radians(model_params['pa'] + 90)
+        r_out_arcsec = model_params[['r_in', 'd_r']].sum() / 9.725
+        x_proj = r_out_arcsec * -np.cos( proj_angle ) # neg b/c RA is left-positive
+        y_proj = r_out_arcsec *  np.sin( proj_angle ) 
+        plotter.axes[-1].plot([-x_proj, x_proj], [-y_proj, y_proj], c='k', ls=':', lw=2)
+        plotter.axes[-1].plot(0, 0, marker='*', markersize=7, markeredgewidth=1, color='k')
+        
+        plotter.save(run.name+'/' + run.name + '_bestfit_concise.pdf', dpi=1000)
+        # plotter.show()
+    
+    # else:
+    #     fig = plotting.Figure(layout=(4,3),
+    #        paths=[[obs, path + '.fits', path + '.residuals.fits']
+    #            for obs, path in zip(aumic_fitting.band6_fits_images, paths)],
+    #        rmses=[3*[rms] for rms in aumic_fitting.band6_rms_values],
+    #        texts=[
+    #            [[[4.6, 4.0, date]],
+    #            [[4.6, 4.0, 'rms={}'.format(np.round(rms*1e6))]],
+    #            None]
+    #            for date, rms in zip(['March', 'August', 'June', 'All'],
+    #            aumic_fitting.band6_rms_values)
+    #            ],
+    #        title= run.name + r'Global Best Fit Model & Residuals',
+    #       savefile=run.name+'/' + run.name + '_bestfit_global.pdf')
           
 
 def sample_disk_flux(run, sample):
@@ -325,4 +339,3 @@ def disk_flux_stats(n_samples):
 
 if __name__ == '__main__':
     main()
-    